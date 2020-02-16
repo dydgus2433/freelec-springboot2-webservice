27e03b5df65be95830ec62e5d243889940928af0
leeyonghyun <dydgus2433@naver.com>
2020-02-17 07:24:50 +0900

    chapter 4 mustache (posts update, delete , list view)

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,9 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    java.util.List<Posts> findAllDesc();
 }
