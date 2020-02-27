1fbcb400bc6251442ecfc1d59d7032232cb5e48f
leeyonghyun <dydgus2433@naver.com>
2020-02-27 15:59:49 +0900

    /profile add and test add

--- src/test/java/com/jojoldu/book/springboot/web/ProfileControllerBootTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/ProfileControllerBootTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.env.MockEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -29,7 +30,7 @@ public class ProfileControllerBootTest {
         String expectedProfile = "default";
 
         ResponseEntity<String > response = restTemplate.getForEntity("/profile", String.class);
-        assertThat(response.getStatusCode()).isEqualTo(expectedProfile);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isEqualTo(expectedProfile);
 
 
